09106b8e9c41748c8c701ba21d2c73f67990aff8
KatharinaMat <katharina.matkevits@gmail.com>
2025-05-12 20:42:02 +0300

    Initial commit

---
+++ src/test/java/tests/LuminorTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
+public class LuminorTest {
+
+    @BeforeClass
+    public void setUp() {
+        Configuration.browser = "chrome";
+    }
+
+    @Test
+    public void searchTest() {
+        open("https://www.imdb.com/");
+        $(byId("suggestion-search")).setValue("Black mirror").pressEnter();
+        $$("li.ipc-inline-list__item").get(0).shouldBe(visible, Duration.ofSeconds(10));
+        $$("li.ipc-inline-list__item").get(0).click();
+        ElementsCollection castMembers = $$("[data-testid='title-cast-item__actor']");
+        for(var i=0; i < Math.min(5, castMembers.size()); i++) {
+            System.out.println(castMembers.get(i).getText());
+        }
+
+    }
+}
